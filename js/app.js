2e5b685b61c9f63e7f7e820dd0e5bfb7d9f4302a
Taeyong Sung <canny0201@gmail.com>
2024-07-21 14:22:24 -0400

    updated to use dom manipulation/added picture to quiz 1

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
-const ButtonOneMainPage = document.querySelector('input1')
+/*------- Cached Element References ------*/
+const firstQuizButtonEl = document.querySelector("#firstQuiz");
+const secondQuizButtonEl = document.querySelector("#secondQuiz");
+const mainPageH1El = document.querySelector(".mainPageH1")
+const messageH2El = document.querySelector("#message")
+const imageOfElephantEl = document.querySelector("#picture")
+/*-------- Functions -----------------*/
+firstQuizButtonEl.addEventListener("click", () => {
+    console.log("clicked me")
+    mainPageH1El.textContent = "Guess the correct animal!"
+    messageH2El.textContent = ''
+    firstQuizButtonEl.textContent = 'Elephant'
+    secondQuizButtonEl.textContent = 'Tiger'
+    imageOfElephantEl.src = "../assets/elephant.webp"
+});
+
+secondQuizButtonEl.addEventListener("click", () => {
+    console.log("clicked mee")
+});
+
+function render() {
+    refreshPage()
+}
+// This is where adding a image tag to HTML making it hidden and clicking button unhides it
+function refreshPage() {
+    mainPageH1El.textContent = "Hello"
+}
+
 
