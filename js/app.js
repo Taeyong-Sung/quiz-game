250a1fcec251b5a8915f60c5c383b903ba2ba709
Taeyong Sung <canny0201@gmail.com>
2024-07-24 16:35:41 -0400

    moved data to data.js

--- js/app.js
+++ js/app.js
@@ -1,114 +1,5 @@
 // /*-------------------------------- Import -----------------------------------*/
-// import { quizOne,quizTwo } from "./data.js"
-
-const image = new Image();
-// alt = image.setAttribute("alt", question.alt)
-const quizOne = [
-  {
-    image: (image.src = "../assets/5ermany.png"),
-    // alt: (image.alt = "German Flag"),
-    question: "What is the capital of Germany?",
-    answers: [
-      { text: "Berlin", correct: true },
-      { text: "Munich", correct: false },
-      { text: "Frankfurt", correct: false },
-      { text: "London", correct: false },
-    ],
-  },
-  {
-    image: (image.src = "../assets/italy.png"),
-    question: "What is the capital of Italy?",
-    answers: [
-      { text: "Venice", correct: false },
-      { text: "Rome", correct: true },
-      { text: "Paris", correct: false },
-      { text: "New York", correct: false },
-    ],
-  },
-  {
-    image: (image.src = "../assets/canada.png"),
-    question: "What is the capital of Canada?",
-    answers: [
-      { text: "Vancouver", correct: false },
-      { text: "Toronto", correct: false },
-      { text: "Ottawa", correct: true },
-      { text: "Washington", correct: false },
-    ],
-  },
-  {
-    image: (image.src = "../assets/america.webp"),
-    question: "What is the capital of America?",
-    answers: [
-      { text: "Florida", correct: false },
-      { text: "California", correct: false },
-      { text: "New york", correct: false },
-      { text: "Washington, D.C", correct: true },
-    ],
-  },
-  {
-    image: (image.src = "../assets/japan.jpg"),
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Kyoto", correct: false },
-      { text: "Tokyo", correct: true },
-      { text: "Osaka", correct: false },
-      { text: "Hokkaido", correct: false },
-    ],
-  },
-];
-
-const quizTwo = [
-  {
-    image: (image.src = "../assets/pickachu.png"),
-    question: "What is the name of this Pokemon?",
-    answers: [
-      { text: "Pikachu", correct: true },
-      { text: "Bulbasaur", correct: false },
-      { text: "Jigglypuff", correct: false },
-      { text: "Machop", correct: false },
-    ],
-  },
-  {
-    image: (image.src = "../assets/eevee.png"),
-    question: "What is the name of this Pokemon?",
-    answers: [
-      { text: "Abra", correct: false },
-      { text: "Eevee", correct: true },
-      { text: "Ekans", correct: false },
-      { text: "Poliwhirl", correct: false },
-    ],
-  },
-  {
-    image: (image.src = "../assets/meowth.jpg"),
-    question: "What is the name of this Pokemon?",
-    answers: [
-      { text: "Muk", correct: false },
-      { text: "Doduo", correct: false },
-      { text: "Meowth", correct: true },
-      { text: "Persian", correct: false },
-    ],
-  },
-  {
-    image: (image.src = "../assets/charizard.png"),
-    question: "What is the name of this Pokemon?",
-    answers: [
-      { text: "Ponyta", correct: false },
-      { text: "Charmander", correct: false },
-      { text: "Caterpie", correct: false },
-      { text: "Charizard", correct: true },
-    ],
-  },
-  {
-    image: (image.src = "../assets/snorlex.jpg"),
-    question: "What is the name of this Pokemon?",
-    answers: [
-      { text: "Slowbro", correct: false },
-      { text: "Snorlax", correct: true },
-      { text: "Seel", correct: false },
-      { text: "Dragonite", correct: false },
-    ],
-  },
-];
+import { image, quizOne, quizTwo } from "./data.js"
 /*-------------------------------- Constants --------------------------------*/
 const correctAnswerSound = new Audio("../assets/clapping.wav");
 const wrongAnswerSound = new Audio("../assets/wonrgAnswer.wav");
@@ -189,6 +80,7 @@ function handleSelectCategory(quizSelect) {
 
 function renderQuestion() {
   imageEl.src = currentQuestions[currentQuestionIdx].image;
+  imageEl.alt = currentQuestions[currentQuestionIdx].alt;
   questionEl.textContent = currentQuestions[currentQuestionIdx].question;
   option1El.textContent = currentQuestions[currentQuestionIdx].answers[0].text;
   option2El.textContent = currentQuestions[currentQuestionIdx].answers[1].text;
